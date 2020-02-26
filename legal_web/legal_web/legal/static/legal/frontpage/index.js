53ff9de9eedc07b280e5afa726a38ce467a76da0
PoojaK97 <thisispooja97@gmail.com>
2020-02-26 20:30:19 +0530

    .

--- legal_web/legal_web/legal/static/legal/frontpage/index.js
+++ legal_web/legal_web/legal/static/legal/frontpage/index.js
@@ -5,108 +5,6 @@ var $ = document.querySelector.bind(document);
 var App = {};
 App.init = function () {
 
-	// ************************ Drag and drop ***************** //
-let dropArea = document.getElementById("drop-area")
-
-// Prevent default drag behaviors
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)   
-  document.body.addEventListener(eventName, preventDefaults, false)
-})
-
-// Highlight drop area when item is dragged over it
-;['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
-
-// Handle dropped files
-dropArea.addEventListener('drop', handleDrop, false)
-
-function preventDefaults (e) {
-  e.preventDefault()
-  e.stopPropagation()
-}
-
-function highlight(e) {
-  dropArea.classList.add('highlight')
-}
-
-function unhighlight(e) {
-  dropArea.classList.remove('active')
-}
-
-function handleDrop(e) {
-  var dt = e.dataTransfer
-  var files = dt.files
-
-  handleFiles(files)
-}
-
-let uploadProgress = []
-let progressBar = document.getElementById('progress-bar')
-
-function initializeProgress(numFiles) {
-  progressBar.value = 0
-  uploadProgress = []
-
-  for(let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0)
-  }
-}
-
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  console.debug('update', fileNumber, percent, total)
-  progressBar.value = total
-}
-
-function handleFiles(files) {
-  files = [...files]
-  initializeProgress(files.length)
-  files.forEach(uploadFile)
-  files.forEach(previewFile)
-}
-
-function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
-}
-
-function uploadFile(file, i) {
-  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', url, true)
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-  })
-
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100) // <- Add this
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  })
-
-  formData.append('upload_preset', 'ujpu6gyk')
-  formData.append('file', file)
-  xhr.send(formData)
-}
 	//Init
 	function handleFileSelect(evt) {
 		var files = evt.target.files; // FileList object
@@ -139,31 +37,31 @@ function uploadFile(file, i) {
 	});
 
 	// drop events
-	$("#drop").ondragleave = function (evt) {
-		$("#drop").classList.remove("active");
+	$("#drop-area").ondragleave = function (evt) {
+		$("#drop-area").classList.remove("active");
 		evt.preventDefault();
 	};
-	$("#drop").ondragover = $("#drop").ondragenter = function (evt) {
-		$("#drop").classList.add("active");
+	$("#drop-area").ondragover = $("#drop-area").ondragenter = function (evt) {
+		$("#drop-area").classList.add("active");
 		evt.preventDefault();
 	};
-	$("#drop").ondrop = function (evt) {
+	$("#drop-area").ondrop = function (evt) {
 		$("input[type=file]").files = evt.dataTransfer.files;
 		$("footer").classList.add("hasFiles");
-		$("#drop").classList.remove("active");
+		$("#drop-area").classList.remove("active");
 		evt.preventDefault();
 	};
 
-	//upload more
+	//upload more ??
 	$(".importar").addEventListener("click", function () {
 		$(".list-files").innerHTML = "";
 		$("footer").classList.remove("hasFiles");
 		$(".importar").classList.remove("active");
 		setTimeout(function () {
-			$("#drop").classList.remove("hidden");
+			$("#drop-area").classList.remove("hidden");
 		}, 500);
 	});
 
-	// input change
+	//Input Change
 	$("input[type=file]").addEventListener("change", handleFileSelect);
 }();
